0c779def8ef4ccd4714f312f4590927af758c5a0
Jose  luis Villa <vsjoseluisvs@gmail.com>
2022-04-19 22:50:36 -0500

    Form with style

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,8 @@
 import { useLayoutEffect, useState } from "react";
 import { getRandomInt } from './helpers/getRandomInt';
 
+import './Home.css';
+
 interface ICoordinates {
   height: number;
   width: number;
@@ -27,19 +29,39 @@ const Home = () => {
     })
   }
 
-  return (
-    <div className="App">
-      <h2>Hola :D</h2>
+/**
+ * TO DO: crear rutas y tomar los valores del mensaje de ahi
+ */
 
-      <button
-        style={{
-          position: 'absolute',
-          top: height,
-          right: width
-        }}
-        onMouseOver={ moveButtonRandomly }
-      >No</button>
+  return (
+    <div className="form-container">
+      <form className="register-form">
+        <input 
+          id="declaration-message"
+          className="form-field"
+          type="text"
+          placeholder="Write a message for your crush"
+          name="declarationMessage"
+          value="¿Quieres ser mi novia?"
+        />
+
+        <button className="form-field" type="submit">
+          Create link
+        </button>
+      </form>
     </div>
+    // <div className="home">
+    //   <h2>Hola :D</h2>
+
+    //   <button
+    //     style={{
+    //       position: 'absolute',
+    //       top: height,
+    //       right: width
+    //     }}
+    //     onMouseOver={ moveButtonRandomly }
+    //   >No</button>
+    // </div>
   );
 }
 
